23538019e6e701787bc4bec66c9b2d168b4fb3ad
Rafał Giemza <rafalgiemza6677@gmail.com>
2019-09-21 14:37:40 +0200

    MainActivity layout and CurrentSongPlayingActivity layout

---
+++ app/src/main/java/com/example/musicplayer/CurrentSongPlayingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentSongPlayingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_song_playing)
+    }
+}
